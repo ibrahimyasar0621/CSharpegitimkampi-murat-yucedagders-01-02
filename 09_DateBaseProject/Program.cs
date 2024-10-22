273f47fd2499915c9ddcc4df39b0fb011d614b1f
ibrahimyasar2106 <ibrahimyasar724@gmail.com>
2024-10-22 18:03:02 +0300

    Ders9 Murat yucedag database SQL ile tablo yapma

---
+++ 09_DateBaseProject/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09_DateBaseProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ADO.net   SQL Server Management Studio da taplo yaptık. sorgu yaptık onu burada çağıracağız
+
+            Console.WriteLine("******C# Ürün - Ktegori Bilgi Sistemi");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string tableNumber;
+
+            Console.WriteLine("-----------------------------------------------------------------------");
+            Console.WriteLine("1-Kategoriler");
+            Console.WriteLine("2-Ürünler");
+            Console.WriteLine("3-Siparişler");
+            Console.WriteLine("4-Çıkış Yap");
+            Console.Write("Lütfen Getirmek istediğiniz tablo numarasını girin: ");
+            Console.ReadLine();
+            Console.WriteLine("-----------------------------------------------------------------------");
+
+            // Bizim önce SQL Bağlantısına ihtiyacımız var. 
+
+            SqlConnection connection = new SqlConnection("Data Source=DESKTOP-C4VKPFC;initial Catalog=EgitimKampiDB;integrated security=true");    // Sınıflara erişim sağlamak için bir nesne örneği türetmen lazım. Nesne örneği türetme işlemini bununla yaptık.
+            // (   Veri Tabanı sunucu adresimiz ; Veri Tabanı ismi  ; güvenlik   )
+                                                             // İlk yazdımız SQL connection bir sınıf. 
+                                                             // connection bir nesne.
+                                                             // Bu satırın altındakilerde properties-nitelik. 
+            connection.Open();        //connection ile oluşturduğumuz bağlantıyı açtık.Bağlantımı açtım.
+            SqlCommand command = new SqlCommand("select * From TblCategory" , connection);  // Command isminde nesne örnekliyoruz. tblcategory tablomuz hangi veri tabanında olduğunu bildirmek için connection verdik yanına.
+            SqlDataAdapter adapter = new SqlDataAdapter(command);                     // C# ile SQL arasındna köprü görevi görecek sınıf. // Adapter adında nesne örneği oluşturduk buda command ile çalışacak.
+            DataTable dataTable = new DataTable();   // Data table verilerimi geçici RAM belleğe almamı sağlıcak. 
+            adapter.Fill(dataTable);     // sorguyu RAM bellekte göstermek amacıyla yaptım. 
+
+            connection.Close();   // Bağlantıyı kapattık.
+
+            foreach (DataRow row in dataTable.Rows)   // data row data tableden gelen bir sınıf türü 
+            {
+               foreach (var item in row.ItemArray)    // row dan gelen dizinin itemini yani içeriğini okuyacak.
+                {
+                    Console.WriteLine(item.ToString());
+                }
+               Console.WriteLine();
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ 09_DateBaseProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("09_DateBaseProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("09_DateBaseProject")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5afbb515-cc9b-4e2c-8c37-7e1cb6a2c408")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
